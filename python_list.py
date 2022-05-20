f6edd1e6f7b7ffed008059d5644d08062c055199
池恩 <1150750591@qq.com>
2022-05-20 17:26:52 +0800

    05/20/2022 python_study

--- python_list.py
+++ python_list.py
@@ -48,5 +48,28 @@ STT.pop(2)
 print(STT)
 
 
+#班上某次考试，['Alice', 'Bob', 'Candy', 'David', 'Ellena'] 的成绩分别是 89, 72, 88, 79, 99，
+# 请按照成绩高低，重新排列list中同学名字的顺序。
+S_N = ['Alice', 'Bob', 'Candy', 'David', 'Ellena']
+S_C = [89, 72, 88, 79, 99]
+S_N_C = list(zip(S_C,S_N))
+S_N_C.sort(reverse=True)
+print(S_N_C)
 
+#有三个长方体，他们的长宽高分别是[1, 2, 3], [5, 3, 2], [7, 3, 2]，定义在数组L中，L = [[1, 2, 3], [5, 3, 2], [7, 3, 2]]，
+# 请分别求出三个长方体的表面积。
+L = [[1, 2, 3], [5, 3, 2], [7, 3, 2]]
+S1 = 2*(L[0][0]*L[0][1]+L[0][0]*L[0][2]+L[0][1]*L[0][2])
+S2 = 2*(L[1][0]*L[1][1]+L[1][0]*L[1][2]+L[1][1]*L[1][2])
+S3 = 2*(L[2][0]*L[2][1]+L[2][0]*L[2][2]+L[2][1]*L[2][2])
+print(S1,S2,S3)
+
+# 2
+L = [[1,2,3], [5, 3, 2], [7,3,2]]
+for cube in L:
+    length = cube[0]
+    width = cube[1]
+    height = cube[2]
+    result = length * width * 2 + width * height * 2 + length * height * 2
+    print(result)
 
