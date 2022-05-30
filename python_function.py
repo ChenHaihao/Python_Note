7087f417e6a8c35baefc2c85b0dbeae1cdccfe4d
池恩 <1150750591@qq.com>
2022-05-30 17:18:54 +0800

    05/30/2022 mooc_python_study

--- python_function.py
+++ python_function.py
@@ -53,13 +53,68 @@ print('奇数和:{}'.format(sum[1]))
 
 
 #请分别使用循环和递归的形式定义函数，求出1~100的和。
-#循环
-def cycle_sum(num_l):
-    n = 0
-    if n <= 100 :
-        sum = n + (n+1)
-    n += 1
-    return sum
-print(sum)
-
 #递归
+def sum(num):
+    if num == 1:
+        return 1
+    return num + sum(num - 1)
+print(sum(100))
+#for循环
+def cycle_sum(x,y):
+    sum = 0
+    for n in range(x,y):
+        sum = sum + n
+    return sum
+print(cycle_sum(1,101))
+#while循环
+def cycle_sum2(n):
+    num = 1
+    sum = 0
+    while num <= n:
+        sum = sum +num
+        num = num + 1
+    return sum
+print(cycle_sum2(100))
+
+
+#请实现函数func，当参数类型为list时，返回list中所有数字类型元素的和，当参数类型为tuple时，返回tuple中所有数字类型元素的乘积。
+def func(L):
+    if isinstance(L, list):
+        sum1 = 0
+        for n in L :
+            if isinstance(n,int) or isinstance(n,float):
+                sum1 += n
+        return sum1
+    elif isinstance(L, tuple):
+        sum1 = 1
+        for n in L :
+            if isinstance(n,int) or isinstance(n,float):
+                sum1 *= n
+        return  sum1
+print(func((1,2,3,4,5,6,7,'a','b')))
+
+##我们来定义一个计算 x 的N次方的函数:
+def power(x, n):   # power(x,y) 计算乘幂,x为底数，y为指数
+    s = 1
+    while n > 0:
+        n = n - 1
+        s = s * x
+    return s
+
+#请定义一个 greet() 函数，它包含一个默认参数，如果没有传入参数，打印 Hello, world.，
+# 如果传入参数，打印Hello, 传入的参数内容.
+def greet(n = "world."):
+    print("Hello,"+n)
+greet()
+greet("my_python")
+
+#假设我们要计算任意个数的平均值，就可以定义一个可变参数：
+def avgenum(*args):
+    sum = 0
+    for item in args:
+        sum += item
+        avg = sum/len(args)
+    return avg
+print(avgenum(1,2,3,4,5))
+
+#编写一个函数，它接受关键字参数names，gender，age三个list，分别包含同学的名字、性别和年龄，请分别把每个同学的名字、性别和年龄打印出来。
